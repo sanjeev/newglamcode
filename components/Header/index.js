3757409bedcdcbd8209e00cd373b534dc3c1b822
sanjeev <60956846+sanjeev@users.noreply.github.com>
2022-12-20 22:55:30 +0530

    20

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,10 +4,47 @@ import Image from 'next/image'
 import Logo from "../../glamcode.png"
 import Dropdown from 'react-bootstrap/Dropdown';
 import Modalpup from '../Modal/loction';
+import styled from "styled-components";
+
+const Nav = styled.div`
+background: #15171c;
+height: 80px;
+display: flex;
+justify-content: flex-start;
+align-items: center;
+`;
+
+// const NavIcon = styled()`
+// margin-left: 2rem;
+// font-size: 2rem;
+// height: 80px;
+// display: flex;
+// justify-content: flex-start;
+// align-items: center;
+// `;
+
+const SidebarNav = styled.nav`
+background: linear-gradient(197.24deg,rgba(197,72,200,.95),rgba(240,155,184,.97) 38.62%,#ecd498) !important;
+}
+width: 80%;
+height: 100vh;
+display: flex;
+justify-content: center;
+position: fixed;
+top: 0;
+left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
+transition: 350ms;
+z-index: 10;
+`;
+const SidebarWrap = styled.div`
+width: 100%;
+`;
 function Header() {
     const [showResults, setShowResults] = useState(false);
     const onClick = () => setShowResults(value => !value);
     const [modalShow, setModalShow] = useState(false);
+    const [sidebar, setSidebar] = useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
     return (
         <>
             {localStorage.getItem('devise') === 'D' ? (
@@ -70,6 +107,7 @@ function Header() {
                                 <li className="list-item">
                                     <a
                                         area-label="mobile menu offcanvas svg icon"
+                                        onClick={showSidebar}
                                         className="btn btn--size-33-33 btn--center btn--round btn--color-radical-red btn--bg-white btn--box-shadow main-menu offcanvas-toggle offside-menu"
                                     >
                                         <svg
@@ -132,6 +170,11 @@ function Header() {
                             </ul>
                             <div className='selectloction'>
                                 <div>
+                                    <i
+                                        className=" p-1 fa fa-map-marker"
+                                        style={{ fontSize: 18 }}
+                                        aria-hidden="true"
+                                    />
                                     <button onClick={() => setModalShow(true)} className="dropdown-toggle dropdownborder" type="button" id="dropdownMenuButton" >
                                         {localStorage.getItem("cityname") ? localStorage.getItem("cityname") : 'Select your Location'}
                                     </button>
@@ -143,6 +186,109 @@ function Header() {
                         </div>
 
                     </Container>
+                    <SidebarNav sidebar={sidebar}>
+                        <SidebarWrap>
+                            <button onClick={showSidebar} class="offcanvas-close" aria-label="offcanvas svg icon"><svg style={{ verticalAlign: 'middle' }} xmlns="http://www.w3.org/2000/svg" width="5.973" height="10.449" viewBox="0 0 5.973 10.449"><path id="Icon_ionic-ios-arrow-back" data-name="Icon ionic-ios-arrow-back" d="M13.051,11.417,17,7.466a.747.747,0,0,0-1.058-1.054l-4.479,4.476a.745.745,0,0,0-.022,1.03l4.5,4.507A.747.747,0,1,0,17,15.37Z" transform="translate(-11.251 -6.194)"></path></svg><div></div></button>
+                            <div className="offcanvas-header flex-end">
+                                <div className="logo imagelogo-sidemenu " style={{ margin: 'auto', paddingTop: '23px' }}>
+                                    <a href="/">
+
+                                        <Image className="img-fluid" src={Logo} alt="Glamcode" width="120" height="26" />
+                                    </a>
+                                </div>
+                                <button onClick={showSidebar} className="offcanvas-close" aria-label="offcanvas svg icon">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="5.973"
+                                        height="10.449"
+                                        viewBox="0 0 5.973 10.449"
+                                    >
+                                        <path
+                                            id="Icon_ionic-ios-arrow-back"
+                                            data-name="Icon ionic-ios-arrow-back"
+                                            d="M13.051,11.417,17,7.466a.747.747,0,0,0-1.058-1.054l-4.479,4.476a.745.745,0,0,0-.022,1.03l4.5,4.507A.747.747,0,1,0,17,15.37Z"
+                                            transform="translate(-11.251 -6.194)"
+                                        />
+                                    </svg>
+                                    <div />
+                                </button>
+                            </div>
+                            <div className="offcanvas-mobile-menu-wrapper">
+                                <div className="mobile-menu-bottom">
+                                    <div className="offcanvas-menu">
+                                        <ul>
+                                            <li>
+                                                <a href="/" className="side-menu-box">
+                                                    <span className="side-text">
+                                                        <i className="fa fa-home  icon-m" aria-hidden="true" />
+                                                        Home
+                                                    </span>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="/blog" className="side-menu-box">
+                                                    <span className="side-text">
+                                                        <i className="fa fa-file-text  icon-m" aria-hidden="true" />
+                                                        Blogs
+                                                    </span>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="/about" className="side-menu-box">
+                                                    <span className="side-text">
+                                                        <i className="fa fa-info-circle  icon-m" aria-hidden="true" />
+                                                        About us
+                                                    </span>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="/" className="side-menu-box">
+                                                    <span className="side-text">
+                                                        <i className="fa fa-comment  icon-m" aria-hidden="true" />
+                                                        Contact us
+                                                    </span>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="/" className="side-menu-box">
+                                                    <span className="side-text">
+                                                        <i className="fa fa-user  icon-m" aria-hidden="true" />
+                                                        My Account
+                                                    </span>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="/" className="side-menu-box">
+                                                    <span className="side-text">
+                                                        <i className="fa fa-star  icon-m" aria-hidden="true" />
+                                                        Membership
+                                                    </span>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="/" className="side-menu-box">
+                                                    <span className="side-text">
+                                                        <i className="fa fa-share  icon-m" aria-hidden="true" />
+                                                        Refer And Earn
+                                                    </span>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <div className="side-menu-box">
+                                                    <span className="side-text">
+                                                        <i className="fa fa-sign-in  icon-m" aria-hidden="true" />
+                                                        Logout
+                                                    </span>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </SidebarWrap>
+
+
+                    </SidebarNav>
                 </>
             )
             }
