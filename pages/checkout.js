cdac54e66a48e800b4fdbfdd6b4e3cb92841dca7
sanjeev <60956846+sanjeev@users.noreply.github.com>
2023-01-04 20:28:31 +0530

    3

--- pages/checkout.js
+++ pages/checkout.js
@@ -4,9 +4,10 @@ import { useRouter } from 'next/router'
 import { Datepicker, DatepickerEvent } from "@meinefinsternis/react-horizontal-date-picker";
 import { enUS } from "date-fns/locale";
 import Datedata from "./datedata";
+import moment from 'moment';
 function Checkout() {
     const router = useRouter()
-
+    console.log(moment().toString());
     return (<>
         <div className="servicedesk-bg" style={{ paddingBottom: '50px' }}>
             <div className="header-css-head">
@@ -22,11 +23,18 @@ function Checkout() {
             <div className='mt-5'>
 
                 <Datedata />
+                <div className="date_sec" >
+                    {[...Array(30)].map((elementInArray, index) => (
 
-                {[...Array(n)].map((elementInArray, index) => (
-                    <div className="" key={index}> Whatever needs to be rendered repeatedly... </div>
-                )
-                )}
+                        <div class="date-card" key={index}>
+                            <span class="day">{moment(moment().add(index, 'days').toString()).format('dddd').toString()}</span>
+                            <span class="date"> {moment(moment().add(index, 'days').toString()).format("DD").toString()}</span>
+                            <span class="month"> {moment(moment().add(index, 'days').toString()).format("MMMM").toString()}</span>
+                        </div>
+
+                    )
+                    )}
+                </div>
             </div>
 
         </div>
